e7cc04747b6ccf3b601fd9b855c12803d5b7c1e2
zaidsh98 <sha7rouri29@gmail.com>
2021-08-04 15:44:07 +0300

    lab09

--- models/forcast.js
+++ models/forcast.js
@@ -1,7 +1,43 @@
+const axios = require('axios');
+
+
 class Forecast {
     constructor(weather){
         this.date=weather.valid_date;
         this.description = weather.weather.description
     }
 }
-module.exports = Forecast;
+async function getWeather(req, res) {
+    const city = req.query.cityName
+    const lon = req.query.lon
+    const lat = req.query.lat
+    const URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&lat=${lat}&lon=${lon}&key=${process.env.API_KEY}`;
+
+
+    axios
+        .get(URL)
+        .then(result => {
+            let weatherArray = result.data.data
+            res.send(wetherObject(weatherArray));
+        })
+        .catch(err => {
+            res.send(err);
+        })
+}
+
+const wetherObject = (weatherObj) => {
+
+    const forecastObj = [];
+    weatherObj.map(element => {
+
+        const description = `Low of ${element.low_temp} ,High of ${element.max_temp} with ${element.weather.description}`;
+        const date = element.datetime;
+        forecastObj.push(new Forecast(description, date));
+    });
+    return forecastObj;
+};
+
+module.exports = {
+    getWeather,
+    wetherObject,
+}
--- models/movies.js
+++ models/movies.js
@@ -1,12 +1,39 @@
-class Movies {
-    constructor(title,overview,vote_average,vote_count,poster_path,popularity,release_date) {
-    this.title = title
-    this.overview = overview
-    this.vote_average = vote_average
-    this.vote_count = vote_count
-    this.poster_path = poster_path
-    this.popularity = popularity
-    this.release_date = release_date
-    }
+const axios = require('axios');
+
+const moviesObject = (moviesObj) => {
+
+    const moviesObj = [];
+    moviesObj.map(element => {
+
+    const title = element.title
+    const overview = element.overview
+    const vote_average = element.vote_average
+    const vote_count = element.vote_count
+    const poster_path = process.env.img_url+element.poster_path
+    const popularity = element.popularity
+    const release_date = element.release_date
+
+        moviesObj.push(new Movies(title,overview,vote_average,vote_count,poster_path,popularity,release_date));
+
+       
+    });
+    return moviesObj;
 }
-module.exports = Movies;
+async function getMovie(req, res) {
+    const city = req.query.city
+    const URLMovie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`
+
+    axios.get(URLMovie).then(result => {
+
+            let moviesArray = result.data.results
+
+            res.send(moviesObject(moviesArray));
+        })
+        .catch(err => {
+            res.send(err);
+        })
+}
+module.exports = {
+    getMovie,
+    moviesObject,
+}
